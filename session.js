7abbc0117ec1f1eedf6c752643ea87858fb3c2d6
sowetobadest <132270689+sowetobadest@users.noreply.github.com>
2024-11-21 16:01:38 +0300

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=mUdXBIKJ#ZlsMNCPBc90G7Fr6TwojVStqXqxuzN-G1qqwl4R5JkY",
 };
